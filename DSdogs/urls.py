29d396150476943066708c6469ffe6feb7cbaba8
Danny_from_nb <cooldannyboy@gmail.com>
2015-12-22 17:47:50 +0800

    add product info

--- DSdogs/urls.py
+++ DSdogs/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from products.views import product_info
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^product/[0-9]+', product_info),
     url(r'^$', 'DSdogs.view.index'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response, render
+from products.models import Product
 # Create your views here.
+
+def product_info(request, id):
+    p = Product.object.get(id=id)
+
+    return render_to_response('product_info.html', locals());
